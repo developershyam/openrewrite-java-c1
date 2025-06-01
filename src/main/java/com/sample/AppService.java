5960d2bbd40e8a39325bf71e30e577fc6fa3e6cd
Shyam Pareek <developer.shyampareek@gmail.com>
2025-06-01 13:20:14 +0530

    Test comment stuff

--- src/main/java/com/sample/AppService.java
+++ src/main/java/com/sample/AppService.java
@@ -1,16 +1,49 @@
+// This is before package
 package com.sample;
+// This is before import
+import java.util.Date;
 
+import org.springframework.stereotype.Component;
+// This is before class
 public class AppService {
 
+    /**
+     * This is before field restServiceOld.
+     */
     private RestServiceOld restServiceOld;
 
+    // This is before field date
+    private Date date;
+
+    // This is before constructor AppService
     public AppService(RestServiceOld restServiceOld) {
         this.restServiceOld = restServiceOld;
+        this.date = new Date();
     }
 
-
+    /**
+     * This method is used to test the old API call.
+     */
     public void testAPICAll(){
+        this.date = new Date();
+        System.out.println("Current date: " + this.date);
         System.out.println(this.restServiceOld.oldCall());
     }
     
+}
+
+@Component
+class TestAnotherClass {
+    public void testMethod() {
+        System.out.println("This is a test method in TestAnotherClass.");
+    }
+    
+    public String anotherMethod(String input) {
+        return "Processed: " + input;
+    }
+}
+
+interface InnerAppService {
+    String testVar="Test";
+    void innerMethod();
 }
